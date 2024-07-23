56ad9305a7ffdaadad7bba2844482d2c1f2fb21c
Mathe-Roland <mathe_roland@yahoo.com>
2024-07-23 09:04:42 +0300

    changed head element

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,9 +16,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
 
        <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <meta name="description" content="This is a description of my Next.js app" />
-        <title>Panouri traforate</title>
+        <meta name="description" content="Produse traforate personalizate " />
+        <title>Produse traforate</title>
       </head>
 
     <body className={`height ${inter.className}`}>
