de50b6fec3027321e81b78c97ca63967a1c265c7
Mathe-Roland <mathe_roland@yahoo.com>
2024-07-24 14:18:11 +0300

    modified cloudinary url

--- src/app/components/functions/regexconversion.js
+++ src/app/components/functions/regexconversion.js
@@ -1,5 +1,7 @@
 export const cloudinaryTransformation = (url, width, height) => {
-    const regex = /\/image\/upload\/(.*)\/(.+)/;
-    const newUrl = url.replace(regex, `/image/upload/f_auto,w_${width},h_${height}/$2`);
-    return newUrl;
+
+        const regex = /\/image\/upload\/(.*)\/(.+)/;
+        const newUrl = url.replace(regex, `/image/upload/f_auto,w_${width},h_${height}/$2`);
+        return  url ? newUrl : "";
+
 };
