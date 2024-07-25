8eaaa2c9297c221d5a8e20c09192a2f64a780817
Mathe-Roland <mathe_roland@yahoo.com>
2024-07-25 11:34:50 +0300

    added preload

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ import './globals.css';
 import Foot from './components/footer/Foot';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/navbar';
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,8 +11,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="description" content="Produse traforate vopsite sau nevopsite din lemn, personalizate" />
         <title>Produse traforate personalizate</title>
+        <meta name="description" content="Produse traforate vopsite sau nevopsite din lemn, personalizate" />
+        <link rel='preload' fetchPriority='high' href='https://res.cloudinary.com/ddrkdrrre/image/upload/v1721761368/szobasfafeketer_adfc03d26d.png' as="image"></link>
       </head>
       <body className={`height ${inter.className}`}>
         <Header />
