844160e98d942247665596c4410ba59d44d17171
Mathe-Roland <mathe_roland@yahoo.com>
2024-07-25 11:48:42 +0300

    add new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import './globals.css';
 import Foot from './components/footer/Foot';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/navbar';
+import { cloudinaryTransformation } from './components/functions/regexconversion';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +14,7 @@ export default function RootLayout({ children }) {
       <head>
         <title>Produse traforate personalizate</title>
         <meta name="description" content="Produse traforate vopsite sau nevopsite din lemn, personalizate" />
-        <link rel='preload' fetchPriority='high' href='https://res.cloudinary.com/ddrkdrrre/image/upload/v1721761368/szobasfafeketer_adfc03d26d.png' as="image"></link>
+        <link rel='preload' fetchPriority='high' href={`${cloudinaryTransformation(`https://res.cloudinary.com/ddrkdrrre/image/upload/v1721761368/szobasfafeketer_adfc03d26d.png`,250,250)}`} as="image"></link>
       </head>
       <body className={`height ${inter.className}`}>
         <Header />
