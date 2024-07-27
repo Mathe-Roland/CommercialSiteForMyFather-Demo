c3a0805c23321de0c6e5a8a5d15c7e86cfc5a98b
Mathe-Roland <mathe_roland@yahoo.com>
2024-07-27 12:44:51 +0300

    trying to solve CLS

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/navbar';
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -11,12 +12,10 @@ export default function RootLayout({ children }) {
         <title>Produse traforate personalizate</title>
         <meta name="description" content="Produse traforate vopsite sau nevopsite din lemn, personalizate" />
       </head>
-      <body className="root-layout">
+      <body className={`height`}>
         <Header />
         <Navbar />
-        <main className="content">
-          {children}
-        </main>
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
