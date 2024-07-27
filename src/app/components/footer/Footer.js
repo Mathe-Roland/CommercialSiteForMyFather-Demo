e02a6108dc5bced80fe86a8ca8e7a86d00125f28
Mathe-Roland <mathe_roland@yahoo.com>
2024-07-27 13:04:16 +0300

    restructured footer

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -1,61 +1,56 @@
 import "./Foot.css";
 
-const Footer=()=>{
 
-
-
+const Footer = () => {
     return (
-        <div className="footer-container">
-            <div className="footer-first-part-container">
-                <div className="footer-first-part">
-                <p>Link-uri rapide:</p>
-                <hr></hr>
-
+        <footer className="footer-container">
+            <section className="footer-first-part">
+                <h2>Link-uri rapide:</h2>
+                <hr />
                 <div className="footer-first-part-columns">
+                    <div className="footer-first-part-column">
+                        <ul>
+                            <li>Despre noi</li>
+                            <li>Termeni si Conditii</li>
+                            <li>Confidentialitate</li>
+                            <li>Metode de plata</li>
+                        </ul>
+                    </div>
+                    <div className="footer-first-part-column">
+                        <ul>
+                            <li>Retur produse</li>
+                            <li>Garantia produselor</li>
+                            <li>Intrebari Frecvente</li>
+                            <li>Online Dispute Resolution</li>
+                        </ul>
+                    </div>
+                    <div className="footer-first-part-column">
+                        <ul>
+                            <li>ANPC</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
 
-                <div className="footer-first-part-column">
-                <p>Despre noi</p>
-                <p>Termeni si Conditii</p>
-                <p>Confidentialitate</p>
-                <p>Metode de plata</p>
-                </div>
-
-                <div className="footer-first-part-column">
-                
-                <p>Retur produse</p>
-                <p>Garantia produselor</p>
-                <p>Intrebari Frecvente</p>
-                <p>Online Dispute Resolution</p>
-                </div>
-                <div className="footer-first-part-column none">
-                <p>ANPC</p>
-
-                </div>
-                </div>
-                
-                </div>
-            </div>
-                <div className="footer-second-part">
-
-                <p>Informatii contact:</p>
-                <hr></hr>
+            <section className="footer-second-part">
+                <h2>Informatii contact:</h2>
+                <hr />
                 <div className="footer-second-part-columns">
-                <p className="footer-email">Adresa de e-mail: <span className="black">@apusite@yahoo.com</span></p>
-                <p className="footer-phone">Numar de telefon: <span className="black">@apusite@yahoo.com</span></p>
-                    
+                    <p className="footer-email">
+                        Adresa de e-mail: <span className="black">@apusite@yahoo.com</span>
+                    </p>
+                    <p className="footer-phone">
+                        Numar de telefon: <span className="black">@apusite@yahoo.com</span>
+                    </p>
                 </div>
+            </section>
 
-                </div>
-
-                <div className="footer-third-part">
-                <p>Link-uri rapide:</p>
-                <hr></hr>
-                
-                </div>
-        </div>
-
-    )
-}
-
+            <section className="footer-third-part">
+                <h2>Link-uri rapide:</h2>
+                <hr />
+            </section>
+        </footer>
+    );
+};
 
 export default Footer;
