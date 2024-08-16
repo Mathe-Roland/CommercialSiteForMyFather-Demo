1f25c9c514be964ec3e5725897d1142b3091c3aa
Mathe-Roland <mathe_roland@yahoo.com>
2024-08-16 15:04:31 +0300

    changed api

--- src/app/api/checkout_sessions/route.js
+++ src/app/api/checkout_sessions/route.js
@@ -1,39 +1,30 @@
-
 import { NextResponse } from "next/server";
-
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request) {
+  const response = await request.json();
+  const line_items = response.cart.data.map((e) => ({
+    price_data: {
+      currency: "ron",
+      product_data: { name: e.attributes.title },
+      unit_amount: e.attributes.price * 100,
+    },
+    quantity: e.attributes.quantity,
+  }));
 
-    const response=await request.json();
+  const checkoutSession = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items,
+    mode: 'payment',
+    success_url: `https://www.decorcut.com/payment-success`,
+    cancel_url: `https://www.decorcut.com/`,
+    billing_address_collection: 'required',
+  });
 
-    const line_items=response.cart.data.map((e)=>{
+  const responseHeaders = new Headers();
+  responseHeaders.set('Cache-Control', 's-maxage=3600');
 
-        const line_itemsObject= {
-            price_data:{
-                currency:"ron",
-                product_data:{
-                    name:e.attributes.title,
-                },
-                unit_amount:e.attributes.price*100,
-            },
-            quantity:e.attributes.quantity,
-        };
-
-        return line_itemsObject;
-
-
-    })
-
-    const checkoutSession = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items:line_items,
-        mode: 'payment',
-        success_url: `https://www.decorcut.com/payment-success`,
-        cancel_url: `https://www.decorcut.com/`,
-        billing_address_collection: 'required',
-      });
-      
-
-  return NextResponse.json({ url: checkoutSession.url });
+  return new NextResponse(JSON.stringify({ url: checkoutSession.url }), {
+    headers: responseHeaders,
+  });
 }
