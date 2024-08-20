ab78f0fe6f195b2c0823d42e0764553b942007a4
Mathe-Roland <mathe_roland@yahoo.com>
2024-08-20 18:00:55 +0300

    try at asynchronous loading

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,13 +5,20 @@ import Header from './components/header/Header';
 import Navbar from './components/navbar/navbar';
 
 export default function RootLayout({ children }) {
-  
+    
+    window.addEventListener('load', () => {
+        const script = document.createElement('script');
+        script.src = 'https://js.stripe.com/v3/';
+        script.async = true;
+        document.head.appendChild(script);
+      });
+      
+
     return (
         <html lang="en">
             <head>
                 <title>Produse traforate personalizate</title>
                 <meta name="description" content="Produse traforate vopsite sau nevopsite din lemn, personalizate" />
-                <script async src="https://js.stripe.com/v3/"></script>
                 <link rel="preconnect" href="https://js.stripe.com"/>
                 <link rel="dns-prefetch" href="https://js.stripe.com/"/>
             </head>
