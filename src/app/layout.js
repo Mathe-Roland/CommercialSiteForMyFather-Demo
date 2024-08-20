569c9891a23d631fa410b0e01484a33ec8a779a0
Mathe-Roland <mathe_roland@yahoo.com>
2024-08-20 21:03:59 +0300

    changed to defer

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,12 @@ import Navbar from './components/navbar/navbar';
 
 export default function RootLayout({ children }) {
     
-    if (typeof window !== 'undefined') {
-        const script = document.createElement('script');
-        script.src = 'https://js.stripe.com/';
-        script.async = true;
-        document.head.appendChild(script);
-      }
+    // if (typeof window !== 'undefined') {
+    //     const script = document.createElement('script');
+    //     script.src = 'https://js.stripe.com/v3/';
+    //     script.async = true;
+    //     document.head.appendChild(script);
+    //   }
   
 
 
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
             <head>
                 <title>Produse traforate personalizate</title>
                 <meta name="description" content="Produse traforate vopsite sau nevopsite din lemn, personalizate" />
+                <script defer src="https://js.stripe.com/"></script>
                 <link rel="preconnect" href="https://js.stripe.com"/>
                 <link rel="dns-prefetch" href="https://js.stripe.com/"/>
             </head>
