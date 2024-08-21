1cb2a41c1c9ced14c4ff7937f1f2447200bdd57c
Mathe-Roland <mathe_roland@yahoo.com>
2024-08-21 20:23:03 +0300

    defered script

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,37 +3,37 @@ import './globals.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/navbar';
-import Script from 'next/script';
-import Head from 'next/head';
+
 
 export default function RootLayout({ children }) {
+    
+    if (typeof window !== 'undefined') {
+        const script = document.createElement('script');
+        script.src = 'https://js.stripe.com/v3/';
+        script.defer = true;
+        document.head.appendChild(script);
+      }
+      
+   
     return (
         <html lang="en">
-            <Head>
+            <head>
                 <title>Produse traforate personalizate</title>
                 <meta name="description" content="Produse traforate vopsite sau nevopsite din lemn, personalizate" />
-                <link rel="dns-prefetch" href="https://js.stripe.com" />
-                <link rel="preload" href="https://www.decorcut.com/_next/static/css/a2504fd93a9ed98a.css" as="style" />
-            </Head>
-            <body className="height">
+                <link rel="preconnect" href="https://js.stripe.com"/>
+                <link rel="dns-prefetch" href="https://js.stripe.com/"/>
+                <link rel="dns-prefetch" href="https://js.stripe.com/"/>
+                <link rel="preload" href="https://www.decorcut.com/_next/static/css/a2504fd93a9ed98a.css" as="style"/>
+                <script src="https://www.decorcut.com/_next/static/chunks/fd9d1056-3e5d3c630784c984.js" defer></script>
+                <script src="https://www.decorcut.com/_next/static/chunks/7023-93c943c9116ac439.js" defer></script>
+
+
+            </head>
+            <body className={`height`}>
                 <Header />
                 <Navbar />
                 <main>{children}</main>
                 <Footer />
-                
-                <Script
-                    src="https://js.stripe.com/v3/"
-                    strategy="afterInteractive"
-                />
-
-                <Script
-                    src="https://www.decorcut.com/_next/static/chunks/fd9d1056-3e5d3c630784c984.js"
-                    strategy="lazyOnload"
-                />
-                <Script
-                    src="https://www.decorcut.com/_next/static/chunks/7023-93c943c9116ac439.js"
-                    strategy="lazyOnload"
-                />
             </body>
         </html>
     );
