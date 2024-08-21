b597efa7bff4e78e6409b5d12c10989e9dcc1685
Mathe-Roland <mathe_roland@yahoo.com>
2024-08-21 16:45:55 +0300

    tried resolving issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,12 @@ import Navbar from './components/navbar/navbar';
 
 export default function RootLayout({ children }) {
     
-    if (typeof window !== 'undefined') {
-        const script = document.createElement('script');
-        script.src = 'https://js.stripe.com/v3/';
-        script.async = true;
-        document.head.appendChild(script);
-      }
+    // if (typeof window !== 'undefined') {
+    //     const script = document.createElement('script');
+    //     script.src = 'https://js.stripe.com/v3/';
+    //     script.async = true;
+    //     document.head.appendChild(script);
+    //   }
   
    
     return (
@@ -19,8 +19,9 @@ export default function RootLayout({ children }) {
             <head>
                 <title>Produse traforate personalizate</title>
                 <meta name="description" content="Produse traforate vopsite sau nevopsite din lemn, personalizate" />
+                <script defer src="https://js.stripe.com/v3"></script>
+                <link rel="preconnect" href="https://js.stripe.com"/>
                 <link rel="dns-prefetch" href="https://js.stripe.com/"/>
-                <link rel="dns-prefetch" href="https://app-ece6f1a7-8619-459e-a9f4-1643769a735e.cleverapps.io"/>
             </head>
             <body className={`height`}>
                 <Header />
