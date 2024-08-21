69d83b47d7a909603e6fc0a688870a90545273b3
Mathe-Roland <mathe_roland@yahoo.com>
2024-08-21 16:14:41 +0300

    lazyload

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -1,10 +1,12 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, Suspense, lazy } from 'react';
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import Link from 'next/link';
 import './navbar.css';
-import Acasa from './navbarComponents/acasa';
+
+// Lazy load the Acasa component
+const Acasa = lazy(() => import('./navbarComponents/acasa'));
 
 export const navbarData = {
   items: ["Acasa", "Magazin", "Blog", "Despre Noi", "Contact"]
@@ -13,6 +15,9 @@ export const navbarData = {
 const Navbar = () => {
   const [selectedOption, setSelectedOption] = useState('');
   const [showAcasa, setShowAcasa] = useState(false);
+
+  // Detect screen size using window.innerWidth
+  const isMobile = typeof window !== 'undefined' && window.innerWidth < 768;
 
   const handleMouseEnter = (index) => {
     if (index === 1) {
@@ -36,35 +41,35 @@ const Navbar = () => {
     <div className="navbar">
       <div className="navbar-centered">
         <div className="navbar-contents">
-          {navbarData.items.map((element,index) => (
-            <div 
+          {navbarData.items.map((element, index) => (
+            <div
               key={element} // Use the element itself as the key
-              onMouseEnter={() => handleMouseEnter(navbarData.items.indexOf(element))}
+              onMouseEnter={() => handleMouseEnter(index)}
               onMouseLeave={handleMouseLeave}
             >
               {element === "Acasa" ? (
                 <Link href={"/"}>
-                
-                <p className='navbar-text'>
-                  {element}
-                </p>
+                  <p className='navbar-text'>
+                    {element}
+                  </p>
                 </Link>
-                
-              ) : (element==="Magazin"?
+              ) : (element === "Magazin" ?
                 <p className='navbar-text'>
                   {element}
                 </p>
-              :
+                :
                 <p className='navbar-text'>
                   <Link href={generateUrl(element)}>
                     {element}
                   </Link>
                 </p>
               )}
-              {showAcasa && index === 1 ? (
-                <div className="width100">
-                  <Acasa />
-                </div>
+              {showAcasa && index === 1 && !isMobile ? (
+                <Suspense fallback={<div>Loading...</div>}>
+                  <div className="width100">
+                    <Acasa />
+                  </div>
+                </Suspense>
               ) : null}
             </div>
           ))}
@@ -79,27 +84,29 @@ const Navbar = () => {
             value={selectedOption}
             onChange={handleChange}
           >
-            {navbarData.items.map((e,index) => (
+            {navbarData.items.map((e, index) => (
               e !== "Acasa" ? (
-                index===1?
-                null
-                :
-                <MenuItem 
-                  key={e} 
-                  component="a" 
-                  href={e.toLowerCase().split(" ").join("-")} 
+                index === 1 ?
+                  null
+                  :
+                  <MenuItem
+                    key={e}
+                    component="a"
+                    href={e.toLowerCase().split(" ").join("-")}
+                    value={e}
+                  >
+                    {e}
+                  </MenuItem>
+              ) : (
+                <MenuItem
+                  key={e}
+                  component="a"
+                  href={"/"}
                   value={e}
                 >
                   {e}
                 </MenuItem>
-              ) : (<MenuItem 
-                key={e} 
-                component="a" 
-                href={"/"} 
-                value={e}
-              >
-                {e}
-              </MenuItem>)
+              )
             ))}
           </Select>
         </FormControl>
