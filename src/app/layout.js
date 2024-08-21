8ca492776ca475a7f0e2afc3ece3bfa311195a3c
Mathe-Roland <mathe_roland@yahoo.com>
2024-08-21 16:23:51 +0300

    resolve dns prefectch

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,8 +19,8 @@ export default function RootLayout({ children }) {
             <head>
                 <title>Produse traforate personalizate</title>
                 <meta name="description" content="Produse traforate vopsite sau nevopsite din lemn, personalizate" />
-                <link rel="preconnect" href="https://js.stripe.com"/>
                 <link rel="dns-prefetch" href="https://js.stripe.com/"/>
+                <link rel="dns-prefetch" href="https://app-ece6f1a7-8619-459e-a9f4-1643769a735e.cleverapps.io"/>
             </head>
             <body className={`height`}>
                 <Header />
