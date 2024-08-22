0591938870cce882c126b220dfd6e7ae6eb143cc
Mathe-Roland <mathe_roland@yahoo.com>
2024-08-22 13:19:09 +0300

    loadeded async script

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,29 +6,27 @@ import Navbar from './components/navbar/navbar';
 
 export default function RootLayout({ children }) {
     if (typeof window !== 'undefined') {
-        // Load the CSS files asynchronously
+
         const loadCSS = (href) => {
             const link = document.createElement('link');
             link.rel = 'stylesheet';
             link.href = href;
-            link.media = 'print'; // Loads the CSS file in a non-blocking manner
-            link.onload = () => {
-                link.media = 'all'; // Applies the CSS once it's loaded
-            };
+            link.type = 'text/css';
+            link.media = 'all';
             document.head.appendChild(link);
         };
+    
+        window.addEventListener('load', () => {
+            loadCSS('https://www.decorcut.com/_next/static/css/9af112944cc39123.css');
+            loadCSS('https://www.decorcut.com/_next/static/css/b2df370e7c0c4128.css');
+            loadCSS('https://www.decorcut.com/_next/static/css/bd5aa9cff39a6305.css');
+        });
 
-        loadCSS('https://www.decorcut.com/_next/static/css/a2504fd93a9ed98a.css');
-        loadCSS('https://www.decorcut.com/_next/static/css/bd5aa9cff39a6305.css');
-        loadCSS('https://www.decorcut.com/_next/static/css/b2df370e7c0c4128.css');
-
-        // Add Stripe script
         const stripeScript = document.createElement('script');
         stripeScript.src = 'https://js.stripe.com/v3/';
         stripeScript.defer = true;
         document.head.appendChild(stripeScript);
 
-        // Add other scripts
         const script1 = document.createElement('script');
         script1.src = 'https://www.decorcut.com/_next/static/chunks/fd9d1056-3e5d3c630784c984.js';
         script1.defer = true;
