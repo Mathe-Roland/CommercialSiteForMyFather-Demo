612a4fd98e007555ffcd8d190f5a35d94bb67021
Mathe-Roland <mathe_roland@yahoo.com>
2024-08-22 18:29:15 +0300

    changed message

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,15 +14,6 @@ export default function RootLayout({ children }) {
         stripeScript.defer = true;
         document.head.appendChild(stripeScript);
 
-        const script1 = document.createElement('script');
-        script1.src = 'https://www.decorcut.com/_next/static/chunks/fd9d1056-3e5d3c630784c984.js';
-        script1.defer = true;
-        document.head.appendChild(script1);
-
-        const script2 = document.createElement('script');
-        script2.src = 'https://www.decorcut.com/_next/static/chunks/7023-93c943c9116ac439.js';
-        script2.defer = true;
-        document.head.appendChild(script2);
     };
 
     return (
@@ -31,7 +22,6 @@ export default function RootLayout({ children }) {
                 <title>Produse traforate personalizate</title>
                 <meta name="description" content="Produse traforate vopsite sau nevopsite din lemn, personalizate" />
                 <link rel="dns-prefetch" href="https://js.stripe.com/"/>             
-                <link rel="preconnect" href="https://app-ece6f1a7-8619-459e-a9f4-1643769a735e.cleverapps.io"/>
             </head>
             <body className={`height`}>
                 <Header />
