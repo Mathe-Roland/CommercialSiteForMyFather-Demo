22e4d72ea0dc95e49e9b8457d62eea149cd4e139
Mathe-Roland <mathe_roland@yahoo.com>
2024-09-09 12:22:30 +0300

    aproape terminat footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,31 +6,28 @@ import Navbar from './components/navbar/navbar';
 import Cookies from 'js-cookie';
 import ShowCookiesModal from './components/cookies/Cookies';
 
-
 export default function RootLayout({ children }) {
-    
-        if (typeof window !== 'undefined') {
-            const stripeScript = document.createElement('script');
-            stripeScript.src = 'https://js.stripe.com/v3/';
-            stripeScript.defer = true;
-            document.head.appendChild(stripeScript);
+    if (typeof window !== 'undefined') {
+        const stripeScript = document.createElement('script');
+        stripeScript.src = 'https://js.stripe.com/v3/';
+        stripeScript.defer = true;
+        document.head.appendChild(stripeScript);
+    }
 
-        }
-    
     return (
         <html lang="en">
             <head>
                 <title>panou decorativ traforat</title>
                 <meta name="description" content="Produse traforate vopsite sau nevopsite din lemn, personalizate" />
-                <link rel="dns-prefetch" href="https://js.stripe.com/"/>             
+                <link rel="dns-prefetch" href="https://js.stripe.com/"/>
             </head>
-            <body className={`height`}>
+            <body>
                 <Header />
                 <Navbar />
                 <main>{children}</main>
                 <Footer />
-                {typeof window !== 'undefined' ? !Cookies.get("showModal") ? <ShowCookiesModal /> : null :null}
+                {typeof window !== 'undefined' ? !Cookies.get("showModal") ? <ShowCookiesModal /> : null : null}
             </body>
         </html>
     );
-};
+}
