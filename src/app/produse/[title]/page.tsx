909d469e6da4822c05eac5668c29b8c5f3a79f74
Mathe-Roland <mathe_roland@yahoo.com>
2024-11-23 19:07:49 +0200

    not getting the title data

--- src/app/produse/[title]/page.tsx
+++ src/app/produse/[title]/page.tsx
@@ -1,17 +1,15 @@
 import ProdusCC from "./ProdusCC";
-import { Metadata } from 'next';
+import { Metadata } from "next";
 
 export async function generateMetadata({ searchParams }: { searchParams: { [key: string]: string } }): Promise<Metadata> {
-  const title = searchParams.title.split("-").join(" ");
+  const title = searchParams.title ? searchParams.title.split("-").join(" ") : "Normal Title";
   const description = searchParams.description ? decodeURIComponent(searchParams.description) : "Default Description";
-  console.log(searchParams);
 
   return {
     title,
     description,
   };
 }
-
 
 
 
