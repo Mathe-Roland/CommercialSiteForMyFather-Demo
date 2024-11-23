58683e181f4af93e67e4309f3798d1c8a3104e2a
Mathe-Roland <mathe_roland@yahoo.com>
2024-11-23 18:59:56 +0200

    changed generateMetadata

--- src/app/produse/[title]/page.tsx
+++ src/app/produse/[title]/page.tsx
@@ -1,10 +1,10 @@
 import ProdusCC from "./ProdusCC";
 import { Metadata } from 'next';
 
-
 export async function generateMetadata({ searchParams }: { searchParams: { [key: string]: string } }): Promise<Metadata> {
-  const title = searchParams.title ? searchParams.title.split("-").join(" ") : "Normal Title";
+  const title = searchParams.title.split("-").join(" ");
   const description = searchParams.description ? decodeURIComponent(searchParams.description) : "Default Description";
+  console.log(searchParams);
 
   return {
     title,
