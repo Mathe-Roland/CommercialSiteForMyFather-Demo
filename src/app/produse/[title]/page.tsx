326254b2691fb1006762b6fca137f8fa099e6c7b
Mathe-Roland <mathe_roland@yahoo.com>
2024-11-23 17:18:01 +0200

    small correction

--- src/app/produse/[title]/page.tsx
+++ src/app/produse/[title]/page.tsx
@@ -1,9 +1,9 @@
 import ProdusCC from "./ProdusCC";
-import { headers } from 'next/headers';
-
 import { Metadata } from "next";
 
+
 export async function generateMetadata({ searchParams }: { searchParams: { [key: string]: string } }): Promise<Metadata> {
+  console.log('Search params:', searchParams);
   const title = searchParams.title ? searchParams.title.split("-").join(" ") : "Default Title";
   const description = searchParams.description ? decodeURIComponent(searchParams.description) : "Default Description";
 
