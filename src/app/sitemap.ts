2bc7a35fd6ac6c22213191bdada6589f5519d79f
Mathe-Roland <mathe_roland@yahoo.com>
2026-09-04 20:35:18 +0300

    changed the sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,22 +5,22 @@ export const revalidate = 86400;
 export default async function sitemap() {
   const domain = "https://www.decorcut.ro";
 
-  const [productsRes, blogsRes] = await Promise.all([
+  const [productsRes, blogsRes, categoriesRes] = await Promise.all([
     fetch(
       `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/panouri-traforates?pagination[pageSize]=1000`
     ),
     fetch(
       `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/articles?pagination[pageSize]=1000`
     ),
+    fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/categories?pagination[pageSize]=1000`
+    ),
   ]);
 
   const products = await productsRes.json();
   const blogs = await blogsRes.json();
+  const categories = await categoriesRes.json();
 
-  console.log("PRODUCT COUNT:", products.data.length);
-  console.log("PRODUCT IDS:", products.data.map((item: any) => item.id));
-
-  console.log("BLOG COUNT:", blogs.data.length);
 
   const productUrls = products.data.map((item: any) => ({
     url: `${domain}/produse/${item.id}-${formatForURL(item.attributes.title)}`,
@@ -36,26 +36,22 @@ export default async function sitemap() {
     priority: 0.6,
   }));
 
-  const staticUrls = [
-    "/",
-    "/despre-noi",
-    "/magazin/panouri-decorative",
-    "/magazin/tablouri-gravate",
-    "/magazin/harti",
-    "/magazin/masca-de-calorifer",
-    "/magazin/pandative",
-    "/magazin/cadouri-personalizate",
-    "/blog",
-  ].map((path) => ({
-    url: `${domain}${path}`,
-    lastModified: new Date(),
+  const categoryUrls = categories.data.map((item: any) => ({
+    url: `${domain}/magazin/${formatForURL(item.attributes.category)}`,
+    lastModified: new Date(item.attributes.updatedAt),
     changeFrequency: "weekly" as const,
     priority: 0.7,
-  }));
+}));
 
-  console.log("TOTAL SITEMAP URLS:", 
-    staticUrls.length + productUrls.length + blogUrls.length
-  );
+    const staticUrls = [
+      "/",
+      "/despre-noi",
+      "/blog",
+    ].map((path) => ({
+      url: `${domain}${path}`,
+      changeFrequency: "weekly" as const,
+      priority: 0.7,
+    }));
 
-  return [...staticUrls, ...productUrls, ...blogUrls];
+  return [...staticUrls, ...productUrls, ...blogUrls, ...categoryUrls];
 }
